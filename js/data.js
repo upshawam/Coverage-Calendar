704d38c4bf01be11d4879e613efc55a1e051b041
upshawam <134899406+upshawam@users.noreply.github.com>
2025-10-25 02:46:19 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,12 +1,11 @@
-// data.js
+// js/data.js
 
-// Replace this with your published Apps Script endpoint
-const SHEETS_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjg0a4Xc4slCdXxC-saaIWvHISg8VBbv_Og8YUKOb8_G6BtAx3Qy0N3VWU3hps9B7Z2LCy0dLvp8qnnWbK6VYU0bmowfR6z1sU0wYOHYiHwPq96enFBffkgsjbL2vI1GCGzIMslLti0DgLEq2oaaEKEgbgKjUUQYneimkp7OPozD23w4wympfB4m3PN0Z-bnJc3PkWJizIcuSCaOO8KxUSrQ9dXoidSh3IgUSbnFD_Cj6f5LFMM3duPCELGZjz_rrhUZTBW-mc9s8nvXMU7yHg37938cA&lib=MtyhWyQuWk5oj4-xBXtXtJNQvBsD6Bo-Z";
+const SHEETS_URL = "https://script.google.com/macros/s/AKfycbx_kn7OZsKxSp-7MhZNDP1QMC3OUogRB0LfgA_92JHkOygnMdKH6fv6mcqQsflfeME/exec";
 
 export async function fetchShiftData() {
   try {
-    const res = await fetch(SHEETS_URL);
-    if (!res.ok) throw new Error("Failed to fetch shift data");
+    const res = await fetch(SHEETS_URL, { cache: "no-store" });
+    if (!res.ok) throw new Error(`Fetch failed: ${res.status} ${res.statusText}`);
     const data = await res.json();
     console.log("Fetched shift data:", data);
     return data;
