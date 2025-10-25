7a1781afc0ea6996f522fb212a73ad34fcbe410b
upshawam <134899406+upshawam@users.noreply.github.com>
2025-10-25 07:41:46 +0000

    Resolve conflict in data.js and finalize rebase

--- js/data.js
+++ js/data.js
@@ -1,11 +1,15 @@
 // data.js
 
-// Fetch shift data from JSON (local file or API)
+// Replace this with your published Apps Script endpoint
+const SHEETS_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjg0a4Xc4slCdXxC-saaIWvHISg8VBbv_Og8YUKOb8_G6BtAx3Qy0N3VWU3hps9B7Z2LCy0dLvp8qnnWbK6VYU0bmowfR6z1sU0wYOHYiHwPq96enFBffkgsjbL2vI1GCGzIMslLti0DgLEq2oaaEKEgbgKjUUQYneimkp7OPozD23w4wympfB4m3PN0Z-bnJc3PkWJizIcuSCaOO8KxUSrQ9dXoidSh3IgUSbnFD_Cj6f5LFMM3duPCELGZjz_rrhUZTBW-mc9s8nvXMU7yHg37938cA&lib=MtyhWyQuWk5oj4-xBXtXtJNQvBsD6Bo-Z";
+
 export async function fetchShiftData() {
   try {
     const res = await fetch(SHEETS_URL);
     if (!res.ok) throw new Error("Failed to fetch shift data");
-    return await res.json();
+    const data = await res.json();
+    console.log("Fetched shift data:", data);
+    return data;
   } catch (err) {
     console.error("Error fetching data:", err);
     return {};
